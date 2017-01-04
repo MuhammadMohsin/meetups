7bf984025925bcad8b32ba580dc12a5401795402
Mohsin Khalid <Mohsin Khalid>
2017-01-04 20:23:19 +0500

    Partially implemented about-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { AppComponent } from './components/root.component/app.component';
 import {MaterialModule} from '@angular/material';
+import { AppComponent } from './components/root.component/app.component';
+import { AboutComponent } from './components/about.component/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/about.component/about.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'about-content',
+  templateUrl: './about.component.html',
+  styleUrls: ['./style.scss']
+})
+export class AboutComponent {
+
+}
