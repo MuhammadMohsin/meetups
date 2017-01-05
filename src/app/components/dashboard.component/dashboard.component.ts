7920eab7df2f334f204fd577a3546526d2dc6380
Mohsin Khalid <Mohsin Khalid>
2017-01-05 19:44:41 +0500

    Display contact list on dashboard

--- src/app/components/dashboard.component/dashboard.component.ts
+++ src/app/components/dashboard.component/dashboard.component.ts
@@ -2,8 +2,43 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'dashboard-content',
-  templateUrl: './dashboard.component.html'
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./style.scss']
 })
 export class DashboardComponent {
-
+  contacts = [
+    {name: "Abc", number: "0300-1234567"},
+    {name: "Abc", number: "0300-1234567"},
+    {name: "Abc", number: "0300-1234567"},
+    {name: "Abc", number: "0300-1234567"},
+    {name: "Abc", number: "0300-1234567"},
+    {name: "Abc", number: "0300-1234567"},
+    {name: "Abc", number: "0300-1234567"},
+    {name: "Abc", number: "0300-1234567"},
+    {name: "Abc", number: "0300-1234567"},
+    {name: "Abc", number: "0300-1234567"},
+    {name: "Abc", number: "0300-1234567"},
+    {name: "Abc", number: "0300-1234567"},
+    {name: "Abc", number: "0300-1234567"},
+    {name: "Abc", number: "0300-1234567"},
+    {name: "Abc", number: "0300-1234567"},
+    {name: "Abc", number: "0300-1234567"},
+    {name: "Abc", number: "0300-1234567"},
+    {name: "Abc", number: "0300-1234567"},
+    {name: "Abc", number: "0300-1234567"},
+    {name: "Abc", number: "0300-1234567"},
+    {name: "Abc", number: "0300-1234567"},
+    {name: "Abc", number: "0300-1234567"},
+    {name: "Abc", number: "0300-1234567"},
+    {name: "Abc", number: "0300-1234567"},
+    {name: "Abc", number: "0300-1234567"},
+    {name: "Abc", number: "0300-1234567"},
+    {name: "Abc", number: "0300-1234567"},
+    {name: "Abc", number: "0300-1234567"},
+    {name: "Abc", number: "0300-1234567"},
+    {name: "Abc", number: "0300-1234567"},
+    {name: "Abc", number: "0300-1234567"},
+    {name: "Abc", number: "0300-1234567"},
+    {name: "Abc", number: "0300-1234567"}
+  ]
 }
